8e6879830087d6c6a3ac45a572bf08dd21a2f160
YumingHuo <hym811@outlook.com>
2024-01-16 14:26:55 +0000

    element_plus

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
   <div>
-    <div>
-      <h1>我是app</h1>
-    </div>
+    <h1>我是app</h1>
+    <el-button type="primary" size="default" :icon="Plus">主要按钮</el-button>
+    <el-button type="success" size="small" :icon="Edit">编辑按钮</el-button>
+    <el-button type="danger" size="default" :icon="Delete">删除按钮</el-button>
+    <el-pagination
+      :page-sizes="[100, 200, 300, 400]"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+    />
   </div>
 </template>
 
-<style>
+<script setup lang="ts">
+//引入图标组件
+import { Plus, Edit, Delete } from "@element-plus/icons-vue";
+</script>
 
-</style>
+<style></style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
 
-createApp(App).mount('#app')
+//配置elementplus的语言
+//@ts-ignore
+import zhCn from "element-plus/dist/locale/zh-cn.mjs";
+
+//获取应用实例对象
+const app = createApp(App);
+
+//安装elementplus插件
+app.use(ElementPlus, {
+  locale: zhCn,
+});
+
+//将应用挂载到挂载点上
+app.mount("#app");
